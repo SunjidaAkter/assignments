04ac8b9028d344c4c9c68f13618364c201c4c34e
SunjidaAkter <sanjidabintesayid7@gmail.com>
2023-04-14 10:31:30 +0600

    second problem solved

---
+++ assignment-1/Multiply.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    long long int a,b;
+    scanf("%lld %lld",&a,&b);
+    long long int multiplication=a*b;
+    printf("%lld",multiplication);
+    return 0;
+}

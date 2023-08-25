f4671207b09875f50eb67df3a2a5058481d50b35
SunjidaAkter <sanjidabintesayid7@gmail.com>
2023-08-25 19:52:08 +0600

    mission accomplished 🔥🚀🔥

--- algo/mid_term/Can_Go.cpp
+++ algo/mid_term/Can_Go.cpp
@@ -2,61 +2,40 @@
 using namespace std;
 int n,e;
 typedef pair<int,int> pii;
-const int N=1e3+5;
+const long long int N=1e3+5;
 const int INF=1e9+10;
 vector<pii>adj[N];
 vector<pair<pii,int>>list_of_edges;
 vector <int> dist(N,INF);
 vector<bool>visited(N);
-// void dijkstra(int source){
-//     priority_queue<pii,vector<pii>,greater<pii>>pq;
-//     dist[source]=0;
-//     pq.push({dist[source],source});
-//     while(!pq.empty()){
-//         int u=pq.top().second;
-//         pq.pop();
-//         visited[u]=true;
-//         for(pii vpair:adj[u]){
-//             int v=vpair.first;
-//             int w=vpair.second;
-//             if(visited[v])continue;
-//             if(dist[v]>dist[u]+w){
-//                 dist[v]=dist[u]+w;
-//                 pq.push({dist[v],v});
-//             }
-//         }
-//     }
-// }
-void bellman_ford(int s){
-    for(int i=1;i<=N;i++){
-        dist[i]=INF;
-    }
-    dist[s]=0;
-    for(int i=1;i<N;i++){
-        for(auto edge:list_of_edges){
-            int u=edge.first.first;
-            int v=edge.first.second;
-            int w=edge.second;
-            if(dist[u]!=INF&&dist[v]>dist[u]+w){
+void dijkstra(int source){
+    priority_queue<pii,vector<pii>,greater<pii>>pq;
+    dist[source]=0;
+    pq.push({dist[source],source});
+    while(!pq.empty()){
+        int u=pq.top().second;
+        pq.pop();
+        visited[u]=true;
+        for(pii vpair:adj[u]){
+            int v=vpair.first;
+            int w=vpair.second;
+            if(visited[v])continue;
+            if(dist[v]>dist[u]+w){
                 dist[v]=dist[u]+w;
+                pq.push({dist[v],v});
             }
         }
     }
 }
-void reset(){
-    for(int i=0;i<N;i++){
-        dist[i]=INF;
-        visited[i]=false;
-    }
-}
 int main(){
     cin>>n>>e;
-    while(e--){
+    for(int i=0;i<e;i++){
         int u,v,w;cin>>u>>v>>w;
-        list_of_edges.push_back({{u,v},w});
+        adj[u].push_back({v,w});
+        // adj[v].push_back({u,w});
     }
     int s;cin>>s;
-    bellman_ford(s);
+    dijkstra(s);
     int t;cin>>t;
     while(t--){
         int x,y;cin>>x>>y;

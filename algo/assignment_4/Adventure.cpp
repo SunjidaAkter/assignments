f9d3a7391aae29698ebb034b49f1db74b4e62b3f
SunjidaAkter <sanjidabintesayid7@gmail.com>
2023-09-08 09:18:03 +0600

    mission accomplished 🔥🚀🔥

--- algo/assignment_4/Adventure.cpp
+++ algo/assignment_4/Adventure.cpp
@@ -2,29 +2,29 @@
 using namespace std;
 typedef pair<int,int>pii;
 #define ll long long
-int knapsack(int n,int s,int v[],int w[]){
-    //base case
-    if(n==0||s==0)return 0;
-    if(w[n-1]<=s){
-        int op1=knapsack(n-1,s-w[n-1],v,w)+v[n-1];
-        int op2=knapsack(n-1,s,v,w);
-        return max(op1,op2);
-    }else{
-        return knapsack(n-1,s,v,w);
-    } 
-}
 int main(){
     int t;cin>>t;
     while(t--){
         int n,s;cin>>n>>s;
         int v[n],w[n];
+        for(int i=0;i<n;i++)cin>>w[i];
+        for(int i=0;i<n;i++)cin>>v[i];
+        int dp[n+1][s+1];
         for(int i=0;i<n;i++){
-            cin>>w[i];
+            for(int j=0;j<s;j++){
+                if(i==0||j==0)dp[i][j]=0;
+            }
         }
-        for(int i=0;i<n;i++){
-            cin>>v[i];
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=s;j++){
+                if(w[i-1]<=j){
+                    dp[i][j]=max(dp[i-1][j-w[i-1]]+v[i-1],dp[i-1][j]);
+                }else{
+                    dp[i][j]=dp[i-1][j];
+                }
+            }
         }
-        cout<<knapsack(n,s,v,w)<<endl;
+        cout<<dp[n][s]<<endl;
     }
     return 0;
 }
